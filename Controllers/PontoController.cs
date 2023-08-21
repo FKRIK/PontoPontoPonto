f67727857d1c8cff248769f3f650073fbd36825d
fernando <fernandokrik@hotmail.com>
2023-08-21 18:45:10 -0300

    zuuum

---
+++ Controllers/PontoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PontoAPI;
+
+[ApiController]
+[Route("[controller]")]
+public class PontoController
+{
+    private static List<Ponto> pontos = new List<Ponto>();
+    private static int id = 0;
+
+    [HttpPost]
+    public void CadastraPonto([FromBody] Ponto ponto)
+    {
+        ponto.Id = id++;
+        pontos.Add(ponto);
+        Console.WriteLine(ponto.NomePessoa);
+        Console.WriteLine(ponto.HorarioEntrada);
+        Console.WriteLine(ponto.HorarioSaida);
+    }
+
+    [HttpGet]
+    public IEnumerable<Ponto> ConsultaPontos([FromQuery] int skip = 0, [FromQuery] int take = 25)
+    {
+        return pontos.Skip(skip).Take(take);
+    }
+
+    [HttpGet("{id}")]
+    public Ponto? ConsultaPontoPorId(int id)
+    {
+        return pontos.FirstOrDefault(ponto => ponto.Id == id);
+    }
+}
---
+++ Models/Ponto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PontoAPI;
+public class Ponto
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    public string NomePessoa { get; set; }
+    public string HorarioEntrada { get; set; }
+    public string? HorarioSaida { get; set; }
+
+    public Ponto(string nomePessoa)
+    {
+        NomePessoa = nomePessoa;
+        HorarioEntrada = DateTime.Now.ToString("HH:mm:ss");
+        HorarioSaida = "-";
+    }
+    
+}
